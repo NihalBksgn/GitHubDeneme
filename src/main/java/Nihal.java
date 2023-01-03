2747330f2caecaa6d05fe53ff27a3a72780836b0
GulYALCIN38 <gulfen3@gmail.com>
2023-01-03 11:33:54 +0100

    vgul

--- src/main/java/Nihal.java
+++ src/main/java/Nihal.java
@@ -1,5 +1,6 @@
 public class Nihal {
     public static void main(String[] args) {
         System.out.println("nihal branchi olusturdum");
+        System.out.println("gul branch olustu");
     }
 }
