7ba1299bc8e570370e7eb6af861879c7008c5855
Sumeyye <sumeyyedagdevirozmen@gmail.com>
2023-01-03 14:40:11 +0000

    Sumeyye branchindeki 4.degisiklik

--- src/main/java/Nihal.java
+++ src/main/java/Nihal.java
@@ -2,5 +2,6 @@ public class Nihal {
     public static void main(String[] args) {
 
         System.out.println("nihal branchi olusturdum");
+        System.out.println("Sumeyye Nihal clasinda degisiklik yapti");
     }
 }
