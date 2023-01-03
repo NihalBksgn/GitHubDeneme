e8ca4a3a8b3d0beae560df505c9e12176ab18205
HaticeBilici61 <bilicihatice1987@gmail.com>
2023-01-03 12:14:44 +0100

    deneme

--- src/main/java/Nihal.java
+++ src/main/java/Nihal.java
@@ -1,5 +1,8 @@
 public class Nihal {
     public static void main(String[] args) {
         System.out.println("nihal branchi olusturdum");
+        System.out.println("Hatice branchi olusturuldu");
     }
+
+
 }
