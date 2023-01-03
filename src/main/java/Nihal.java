09a3ccea1876cee3adc16979019b7841385f6f75
FarukDarama <farukdarama2828@gmail.com>
2023-01-03 11:33:59 +0100

    ilk deneme

--- src/main/java/Nihal.java
+++ src/main/java/Nihal.java
@@ -1,5 +1,15 @@
 public class Nihal {
+
+
     public static void main(String[] args) {
+
         System.out.println("nihal branchi olusturdum");
+        System.out.println("kjadkjsdfifjsk");
+
+
+
+
+
+
     }
 }
