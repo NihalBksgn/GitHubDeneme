9283b9eb9b1c129995d3a02dd88fd4518a9223f3
HaticeBilici61 <bilicihatice1987@gmail.com>
2023-01-03 18:22:35 +0100

    branch deyim

--- src/main/java/Faruk.java
+++ src/main/java/Faruk.java
@@ -2,7 +2,7 @@ public class Faruk {
     public static void main(String[] args) {
 
         System.out.println("faruk bunu basaramadı.");
-
+        System.out.println("Hatice branchinda calisiyorum");
 
 
 
