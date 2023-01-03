bd8fc82441d42f15b95c715bf22b14da9ad55fdb
Nihal Bakisgan <nihalkaratasbakisgan@gmail.com>
2023-01-03 14:33:28 +0100

    master 2. ekleme

--- src/main/java/OrtakCalisma.java
+++ src/main/java/OrtakCalisma.java
@@ -1,5 +1,6 @@
 public class OrtakCalisma {
     public static void main(String[] args) {
         System.out.println("ortak calisma alanina ekleme -nihal");
+        System.out.println("master branchine 2.ekleme- nihal");
     }
 }
