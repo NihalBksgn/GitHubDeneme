99992c0f3dce51a07440b09484e8fd1d3c5e89e7
FarukDarama <farukdarama2828@gmail.com>
2023-01-03 11:55:46 +0100

    aa

--- src/main/java/Faruk.java
+++ src/main/java/Faruk.java
@@ -1,8 +1,9 @@
 public class Faruk {
     public static void main(String[] args) {
 
+        System.out.println("nihal branchi olusturdum");
+        System.out.println("kjadkjsdfifjsk");
         System.out.println("faruk bunu basaramadı.");
-
 
 
 
